4dc2232987b3f18146e171f9e51010a7cdc7e136
Sudharsan66 <sudharsan.swaminathan.c@greenwayhealth.com>
2022-02-25 13:13:49 +0530

    Updated help using message handler from button click

--- main.py
+++ main.py
@@ -12,28 +12,41 @@ import voice
 updater = Updater(token="5067785773:AAHikkvebWKMYrbMmoNNWKiUoRGkUUEzUQ0")
 dispatcher = updater.dispatcher
 
+Help = 'Help'
+
 
 def startCommand(update: Update, context: CallbackContext):
     """This is where the bot is initiated - /start command"""
     buttons = [[KeyboardButton('Help')]]
-    context.bot.send_message(chat_id=update.effective_chat.id, text="Welcome to the DuckDuckGo search engine and text to speech bot",
+    context.bot.send_message(chat_id=update.effective_chat.id,
+                             text="Welcome to the DuckDuckGo search engine and text to speech bot",
                              reply_markup=ReplyKeyboardMarkup(buttons))
 
 
 def help(update: Update, context: CallbackContext):
-    """The Help function displays all the available methods and the usages of them"""
-    context.bot.send_message(chat_id=update.effective_chat.id,
-                             text="<b>List of commands:</b>\n\n"
-                                  "<b>/start</b> - \tStarts the bot...!\n"
-                                  "<b>/search</b> - \tTakes an argument and searches the DuckDuckGo Api for that keyword\n "
-                                  "<b>/img</b> - \tTakes an argument and search the DuckDuckGo Api for images of that keyword\n"
-                                  "<b>/random</b> - \tDisplays a random image obtained from picsum api\n"
-                                  "<b>/voice</b> - \ttakes an argument and converts it into audio using python text to speech X3 - pptyx3 - package",
-                             parse_mode=ParseMode.HTML)
+    if update.message.text == 'Help':
+        """The Help function displays all the available methods and the usages of them"""
+        context.bot.send_message(chat_id=update.effective_chat.id,
+                                 text="<b>List of commands:</b>\n\n"
+                                      "<b>/start</b> - \tStarts the bot...!\n"
+                                      "<b>/search</b> - \tTakes an argument and searches the DuckDuckGo Api for that keyword\n "
+                                      "<b>/img</b> - \tTakes an argument and search the DuckDuckGo Api for images of that keyword\n"
+                                      "<b>/random</b> - \tDisplays a random image obtained from picsum api\n"
+                                      "<b>/voice</b> - \ttakes an argument and converts it into audio using python text to speech X3 - pptyx3 - package",
+                                 parse_mode=ParseMode.HTML)
+    else:
+        context.bot.send_message(chat_id=update.effective_chat.id,
+                                 text="<b>List of commands:</b>\n\n"
+                                      "<b>/start</b> - \tStarts the bot...!\n"
+                                      "<b>/search</b> - \tTakes an argument and searches the DuckDuckGo Api for that keyword\n "
+                                      "<b>/img</b> - \tTakes an argument and search the DuckDuckGo Api for images of that keyword\n"
+                                      "<b>/random</b> - \tDisplays a random image obtained from picsum api\n"
+                                      "<b>/voice</b> - \ttakes an argument and converts it into audio using python text to speech X3 - pptyx3 - package",
+                                 parse_mode=ParseMode.HTML)
 
 
 dispatcher.add_handler(CommandHandler("start", startCommand))
-# dispatcher.add_handler(MessageHandler("Help", help))
+dispatcher.add_handler(MessageHandler(Filters.regex(r"Help"), help))
 dispatcher.add_handler(CommandHandler("help", help))
 dispatcher.add_handler(CommandHandler("random", randomImg.random_img))
 dispatcher.add_handler(CallbackQueryHandler(queryHandler.queryHandler))
